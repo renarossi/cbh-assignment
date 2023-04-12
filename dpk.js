f27796143a54b4f3082900d943e8f33e95f1fb53
Renato Rossi <renadlerossi+git@gmail.com>
2023-04-12 16:28:29 -0300

    refactor: Refactor dpk.js.

--- dpk.js
+++ dpk.js
@@ -1,28 +1,23 @@
 const crypto = require("crypto");
 
 exports.deterministicPartitionKey = (event) => {
-  const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
   let candidate;
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
+  if (!event) {
+    return "0"
   }
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
+  if (event && event.partitionKey) {
+    candidate = JSON.stringify(event.partitionKey);
+  } else if (event && !event.partitionKey) {
+    const data = JSON.stringify(event);
+    candidate = crypto.createHash("sha3-512").update(data).digest("hex");
   }
+
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
     candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
   }
+
   return candidate;
 };
